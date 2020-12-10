afd7831d56e83761943dc65d2b547b4e3503060d
Tom Vanwinckel <Tom.Vanwinckel@realdolmen.com>
2020-12-10 10:45:00 +0100

    base url

--- frontend/RMotownFestival/src/environments/environment.prod.ts
+++ frontend/RMotownFestival/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   festivalName: "RMotownFestival",
-  apiBaseUrl: "https://rmotownfestivalapitv.azurewebsites.net/api"
+  apiBaseUrl: "https://rmotownfestivalapitv.azurewebsites.net/api/"
 };
